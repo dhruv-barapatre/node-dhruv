caaf0ffc94cdca4ddf0630643305dda1d35f6c6f
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-10-16 14:19:38 +0530

    update

--- To-Do-List/back/index.js
+++ To-Do-List/back/index.js
@@ -1,4 +1,15 @@
-const express=require("express")
-const server=express()
+const express = require("express")
+const app = express()
 
 // server
+app.use((req, res, next) => {
+    next()
+})
+
+app.get("/", (req, res) => {
+    res.end("ok")
+})
+
+app.listen(8000, () => {
+    console.log("Server Is Running At 8000")
+})
