18ea15aee9415afffb5fd6d0688b67d920dd8e5e
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-10-16 20:09:16 +0530

    update

--- To-Do-List/back/index.js
+++ To-Do-List/back/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const fs=require("fs")
 const app = express()
 
 // server
@@ -6,7 +7,17 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get("/", (req, res) => {
+app.get("/getdata", (req, res) => {
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const newdata=JSON.stringify(data)
+            console.log(data)
+            console.log(JSON.stringify(JSON.parse(data), null, 2));
+
+        }
+    })
     res.end("ok")
 })
 
