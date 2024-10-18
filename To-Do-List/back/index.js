af3dda21bf07027d14c2d0346abd51048fa70dd0
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-10-18 14:28:35 +0530

    update

--- To-Do-List/back/index.js
+++ To-Do-List/back/index.js
@@ -1,15 +1,13 @@
 const express = require("express")
 const fs = require("fs")
 const app = express()
-const cors = require("cors")
+const cors = require("cors");
+const generateUniqueId = require("generate-unique-id");
 
 // server
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("ok")
-})
 app.get("/getdata", (req, res) => {
     fs.readFile("./db.json", "utf-8", (err, data) => {
         if (err) {
@@ -17,7 +15,7 @@ app.get("/getdata", (req, res) => {
             res.send(err)
         } else {
             const { user } = JSON.parse(data)
-            console.log(user)
+            console.log(data)
             const jsondata = JSON.stringify(user)
             res.send(jsondata)
         }
@@ -25,17 +23,19 @@ app.get("/getdata", (req, res) => {
 })
 
 app.post("/addUser", (req, res) => {
-    fs.readFile("./db.json", "utf-8", (err, data) => {
+    fs.readFile("./db.json", "utf-8", (er5r, data) => {
         if (err) {
             res.send(err)
         } else {
             const jsonData = JSON.parse(data)
+            const id = generateUniqueId()
+            req.body.id = id
             jsonData.user.push(req.body)
             const pushData = JSON.stringify(jsonData)
-            fs.writeFile("./db.json",pushData,(err)=> {
-                if(err){
+            fs.writeFile("./db.json", pushData, (err) => {
+                if (err) {
                     res.send(err)
-                }else{
+                } else {
                     res.send("Data Set SuccesFully")
                 }
             })
@@ -43,6 +43,55 @@ app.post("/addUser", (req, res) => {
     })
 
 })
+
+app.delete("/deleteUser/:id", (req, res) => {
+    const { id } = req.params
+    fs.readFile("./db.json", "utf-8", (err, data) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else {
+            const { user } = JSON.parse(data)
+            let deletdUser = user.filter((el) => el.id != id)
+            const pushData = JSON.stringify(deletdUser)
+            fs.writeFile("./db.json", pushData, (err) => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.send("Data Set SuccesFully")
+                }
+            })
+        }
+    })
+})
+
+app.put("/editUser/:id", (req, res) => [
+    fs.readFile("./db.json", "utf-8", (err, data) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else {
+            let { user } = JSON.parse(data)
+            const { id } = req.params
+            const index = user.findIndex((el) => el.id == id)
+            if (index == -1) {
+                res.send("User Not Found")
+            } else {
+                user[index] = req.body
+                user[index].id=id
+                const pushData = JSON.stringify(user)
+                fs.writeFile("./db.json", pushData, (err) => {
+                    if (err) {
+                        res.send(err)
+                    } else {
+                        res.send("Data Set SuccesFully")
+                    }
+                })
+                res.send("ok")
+            }
+        }
+    })
+])
 
 app.listen(8000, () => {
     console.log("Server Is Running At 8000")
