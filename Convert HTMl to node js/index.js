4e37c8861b2518b305fc01675d78c974a47a5cfe
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2025-01-25 14:03:47 +0530

    upda

--- Convert HTMl to node js/index.js
+++ Convert HTMl to node js/index.js
@@ -1,19 +1,19 @@
-const express=require("express");
+const express = require("express");
 const ExpressRouter = require("./routes/Routes");
 const connect = require("./db");
-const app=express();
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-const dotenv=require("dotenv").config()
-app.set("view engine","ejs");
-app.use("/",ExpressRouter);
+const dotenv = require("dotenv").config()
+app.set("view engine", "ejs");
+app.use("/", ExpressRouter);
 
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, async () => {
     try {
-        connect
-             console.log("http://localhost:8080")
+        await connect
+        console.log("Connected With Database")
     } catch (error) {
         console.log(error?.message)
     }
