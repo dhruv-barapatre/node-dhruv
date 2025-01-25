66beb3a1713cd0173bcd41e183e48299822e4291
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2025-01-25 12:47:19 +0530

    update

---
+++ Make api project/client/src/component/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website!</h1>
+      <p>We are excited to have you here. Explore our features and enjoy your experience.</p>
+      <img src="https://img.freepik.com/free-vector/programmers-concept-with-flat-design_23-2147857538.jpg?semt=ais_hybrid" height={400} width={400} alt="Welcome" />
+    </div>
+  );
+};
+export default Home;
